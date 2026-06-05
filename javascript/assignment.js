89befecf45e55d0a74195d778c3be016895fc0bc
Adeola Oluwatoyin ADEWALE <adeola-adewale>
2026-06-05 18:43:38 +0100

    Added JavaScript switch assignment

---
+++ javascript/assignment.js
@@ -0,0 +1,23 @@
+// Student score
+const score = 72;
+
+switch (true) {
+  case (score >= 90):
+    console.log("A — Excellent!");
+    break;
+
+  case (score >= 80):
+    console.log("B — Good");
+    break;
+
+  case (score >= 70):
+    console.log("C — Satisfactory");
+    break;
+
+  case (score >= 60):
+    console.log("D — Needs improvement");
+    break;
+
+  default:
+    console.log("F — Please see your teacher");
+}
---
+++ javascript/index.js
@@ -0,0 +1,21 @@
+// using switch statement
+
+let day = "Sunday";
+
+if (day === "Monday") {
+    console.log ("Today is Monday");
+} else if (day === "Tuesday") {
+    console.log ("Today is Tuesday");
+} else if (day === "Wednesday") {
+    console.log ("Today is Wednesday");
+} else if (day === "Thursday") {
+    console.log ("Today is Thursday");
+} else if (day === "Friday") {
+    console.log ("Today is Friday");
+} else if (day === "Saturday") {
+    console.log ("Today is Saturday");
+} else if (day === "Sunday") {
+    console.log ("Today is Sunday");
+} else {
+    console.log("Invalid day");
+}
